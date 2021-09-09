4520a3280144a71eb4c300d4f2904fac99257d06
thehappycheese <the.nicholas.archer@gmail.com>
2021-09-09 17:50:41 +0800

    Fixed KeepLongest to consider both segment length and value.

--- src/dtimsprep/timestamp.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def timestamp_filename(filename:str):
-	time = datetime.datetime.now().isoformat()
-	date,time = time.split("T")
-	return date.replace("-"," ") +" "+"".join(time.split(":")[:2])+" "+filename
